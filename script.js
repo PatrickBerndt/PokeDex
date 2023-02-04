8ca200d2e17ec95ab70b9c760867dc99b262b2ca
Patrick Berndt <p.berndt@live.de>
2023-02-04 16:15:25 +0100

    added Fullscreen View

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let load = 20;
+let load = 40;
 let currentPokemon;
 
 function init(){
@@ -11,17 +11,66 @@ async function getPokemons(i){
     let url = `https://pokeapi.co/api/v2/pokemon/${i}`;
     let response = await fetch(url);
     let currentPokemon = await response.json();
-    renderMiniCarts(currentPokemon,i);
-    
+    renderMiniCarts(currentPokemon,i);    
 }
+async function callSelection(i){
+    let url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+    let response = await fetch(url);
+    let currentPokemon = await response.json();
+    enterFullscreen(currentPokemon,i);
+}
+
+
 function renderMiniCarts(currentPokemon,i){
+    
     document.getElementById('pokemonNames').innerHTML+=/*html*/`
-       <div class="pokemonMiniCard" id="pokemonMiniCard${i}" onclick="enterFullscreen(${i})">
+       <div class="pokemonMiniCard" id="pokemonMiniCard${i}" onclick="callSelection(${i})">
             <h3> ${currentPokemon['name']}</h3>
+            <div class="pokeId" id="pokeId${i}"></div>
             <img src="${currentPokemon['sprites']['other']['official-artwork']['front_default']}">
             <div class="types" id="type${i}">
             </div>
     `;
+    pokeIndexNr(i);
     addTypeBg(currentPokemon,i);
+    
 }
 
+function pokeIndexNr(i){
+    let str = i.toString();
+    while(str.length<4)str = "0"+str;
+    document.getElementById(`pokeId${i}`).innerHTML= `#${str}`;
+   
+}
+function pokeIndexNrFull(i){
+    let str = i.toString();
+    while(str.length<4)str = "0"+str;
+    document.getElementById(`pokeIdFull${i}`).innerHTML= `#${str}`;
+}
+
+function enterFullscreen(currentPokemon,i){
+
+    document.getElementById('fullscreenView').classList.remove('dNone');
+    document.getElementById('fullscreenView').innerHTML = /*html*/`
+     <div class="fullScreenContainer" id="fullScreenContainer" onclick="event.stopPropagation()">
+        <div class="fullscreenBg" id="fullscreenBg">
+            <h3> ${currentPokemon['name']}</h3>
+           <div class="pokeIdFull" id="pokeIdFull${i}"></div> 
+            <div class="typeBox" id="typeBox"></div>
+        </div>
+        <div class="fullscreenBgbottom"></div>
+            <img src="${currentPokemon['sprites']['other']['official-artwork']['front_default']}">
+        </div>   
+    `;
+    fullscreenBg(currentPokemon);
+    pokeIndexNrFull(i);
+    renderType(currentPokemon,i);
+}
+
+function closeFullScreen(){
+    document.getElementById('fullscreenView').classList.add('dNone');
+}
+
+function renderType(currentPokemon,i){
+    
+}
